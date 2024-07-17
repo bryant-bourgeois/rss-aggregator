56bf1b6187d2a02aa0a9a4e42ebc9a7db508c354
Bryguy76 <bryant.bourgeois@comcast.net>
2024-07-16 21:11:57 -0600

    lazy commit message

--- internal/database/feeds.sql.go
+++ internal/database/feeds.sql.go
@@ -15,7 +15,7 @@ import (
 const createFeed = `-- name: CreateFeed :one
 INSERT INTO feeds (id, created_at, updated_at, name, url, user_id)
 VALUES ($1, $2, $3, $4, $5, $6)
-RETURNING id, created_at, updated_at, name, url, user_id
+RETURNING id, created_at, updated_at, name, url, user_id, last_fetched_at
 `
 
 type CreateFeedParams struct {
@@ -44,6 +44,7 @@ func (q *Queries) CreateFeed(ctx context.Context, arg CreateFeedParams) (Feed, e
 		&i.Name,
 		&i.Url,
 		&i.UserID,
+		&i.LastFetchedAt,
 	)
 	return i, err
 }
@@ -132,7 +133,7 @@ func (q *Queries) ListFeedFollows(ctx context.Context, userID uuid.UUID) ([]User
 }
 
 const listFeeds = `-- name: ListFeeds :many
-SELECT id, created_at, updated_at, name, url, user_id FROM feeds
+SELECT id, created_at, updated_at, name, url, user_id, last_fetched_at FROM feeds
 ORDER BY name
 `
 
@@ -152,6 +153,7 @@ func (q *Queries) ListFeeds(ctx context.Context) ([]Feed, error) {
 			&i.Name,
 			&i.Url,
 			&i.UserID,
+			&i.LastFetchedAt,
 		); err != nil {
 			return nil, err
 		}
--- models.go
+++ models.go
@@ -26,38 +26,50 @@ func databaseUserToUser(user database.User) User {
 }
 
 type Feed struct {
-	ID        uuid.UUID `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	Name      string    `json:"name"`
-	Url       string    `json:"url"`
-	UserID    uuid.UUID `json:"user_id"`
+	ID            uuid.UUID  `json:"id"`
+	CreatedAt     time.Time  `json:"created_at"`
+	UpdatedAt     time.Time  `json:"updated_at"`
+	Name          string     `json:"name"`
+	Url           string     `json:"url"`
+	UserID        uuid.UUID  `json:"user_id"`
+	LastFetchedAt *time.Time `json:"last_fetched_at"`
 }
 
 func databaseFeedToFeed(feed database.Feed) Feed {
+	if !feed.LastFetchedAt.Valid {
+		return Feed{
+			ID:        feed.ID,
+			CreatedAt: feed.CreatedAt,
+			UpdatedAt: feed.UpdatedAt,
+			Name:      feed.Name,
+			Url:       feed.Url,
+			UserID:    feed.UserID,
+		}
+	}
 	return Feed{
-		ID:        feed.ID,
-		CreatedAt: feed.CreatedAt,
-		UpdatedAt: feed.UpdatedAt,
-		Name:      feed.Name,
-		Url:       feed.Url,
-		UserID:    feed.UserID,
+		ID:            feed.ID,
+		CreatedAt:     feed.CreatedAt,
+		UpdatedAt:     feed.UpdatedAt,
+		Name:          feed.Name,
+		Url:           feed.Url,
+		UserID:        feed.UserID,
+		LastFetchedAt: &feed.LastFetchedAt.Time,
 	}
 }
 
 type FeedFollow struct {
-	ID uuid.UUID `json:"id"`
-	UserID uuid.UUID `json:"user_id"`
-	FeedID uuid.UUID `json:"feed_id"`
+	ID        uuid.UUID `json:"id"`
+	UserID    uuid.UUID `json:"user_id"`
+	FeedID    uuid.UUID `json:"feed_id"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
-func databaseFeedFollowToFeedFollow (follow database.UsersFeed) FeedFollow{
-	return FeedFollow {
-		ID: follow.ID,
-		UserID: follow.UserID,
-		FeedID: follow.FeedID,
+func databaseFeedFollowToFeedFollow(follow database.UsersFeed) FeedFollow {
+	return FeedFollow{
+		ID:        follow.ID,
+		UserID:    follow.UserID,
+		FeedID:    follow.FeedID,
 		CreatedAt: follow.CreatedAt,
 		UpdatedAt: follow.UpdatedAt,
 	}
